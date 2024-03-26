68c78464b0abc8c49c19a4bae6e1c6c177cf4362
Marialuisabcs <maria.lbcs@icloud.com>
2024-03-26 18:11:09 -0400

    feat: define connect_timeout to 10 seconds.
    This is needed when using multiple devices connected to the same (local) database.

--- src/sesgx_cli/database/connection.py
+++ src/sesgx_cli/database/connection.py
@@ -3,7 +3,10 @@ from sqlalchemy.orm import sessionmaker
 
 from sesgx_cli.env_vars import DATABASE_URL
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"connect_timeout": 10},
+)
 
 
 Session = sessionmaker(bind=engine, autoflush=False)
