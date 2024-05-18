1af3a3b524af66a53d3da89802ac1338a2200b35
Marialuisabcs <maria.lbcs@icloud.com>
2024-05-18 04:28:37 -0400

    refactor: simplify the results excel export
    Now all the results (including duplicates and n_scopus_results = -1) of each strategy combination are dumped in an excel file, separated in tabs.

--- src/sesgx_cli/cli/results.py
+++ src/sesgx_cli/cli/results.py
@@ -11,21 +11,9 @@ from sesgx_cli.database.util.db_query_execution import (
     get_results_from_db,
     get_strategies_used,
 )
-from sesgx_cli.database.util.results_queries import ResultQuery, SideQueries
-
-_AVAILABLE_METRICS = ["start_set_f1_score", "bsb_recall", "sb_recall"]
-_DEFAULT_METRICS = ["start_set_precision", "start_set_recall"]
+from sesgx_cli.database.util.results_queries import ResultQuery
 
 app = typer.Typer(rich_markup_mode="markdown", help="Get experiments' results.")
-
-
-class InvalidMetric(Exception):
-    """The metric passed as a parameter is not valid"""
-
-
-def verify_metrics(metrics: list[str] | None) -> None:
-    if not set(metrics or []).issubset(set(_AVAILABLE_METRICS)):
-        raise InvalidMetric()
 
 
 def _adjust_max_col_width(df: pd.DataFrame, writer: pd.ExcelWriter, sheet_name: str):
@@ -33,6 +21,7 @@ def _adjust_max_col_width(df: pd.DataFrame, writer: pd.ExcelWriter, sheet_name: 
         max_col_width = max(df[column].astype(str).map(len).max(), len(column))  # type: ignore
         col_idx = df.columns.get_loc(column)
         writer.sheets[sheet_name].set_column(col_idx, col_idx, max_col_width)
+    writer.sheets[sheet_name].set_column(0, 0, 15)
 
 
 def _graph_tab(slr_name: str, excel_writer: pd.ExcelWriter):
@@ -54,70 +43,16 @@ def _graph_tab(slr_name: str, excel_writer: pd.ExcelWriter):
     excel_writer.sheets[sheet_name].set_column(0, 1, 25)
 
 
-def _statistics_tab(
-    results: dict[str, dict], excel_writer: pd.ExcelWriter, strategies: list[str]
-) -> None:
-    root_cols = [
-        "start_set_precision",
-        "start_set_recall",
-        "start_set_f1_score",
-        "bsb_recall",
-        "sb_recall",
-        "n_scopus_results",
-    ]
-    max_cols_highlight = [
-        "mean_start_set_precision",
-        "mean_start_set_recall",
-        "mean_start_set_f1_score",
-        "mean_bsb_recall",
-        "mean_sb_recall",
-    ]
-
-    idx = []
-    for col in root_cols:
-        idx.append(f"mean_{col}")
-        idx.append(f"stdev_{col}")
-
-    stats_df = pd.DataFrame(columns=strategies, index=idx)
-
-    for key, result in results.items():
-        temp_df = pd.DataFrame(data=result["data"], columns=result["columns"])
-        temp_df = temp_df.drop(temp_df[temp_df["n_scopus_results"] <= 0].index)
-
-        for col in root_cols:
-            stats_df.loc[f"mean_{col}", key] = round(temp_df[col].mean(), 5)
-            stats_df.loc[f"stdev_{col}", key] = round(temp_df[col].std(), 5)
-
-    stats_df = stats_df.T
-
-    style_stats = stats_df.style.highlight_max(
-        subset=max_cols_highlight, color="#8aeda4"
-    ).highlight_min(subset=["mean_n_scopus_results"], color="#8aeda4")
-
-    stats_df = stats_df.reset_index()
-    stats_df.rename(columns={"index": "strategies"}, inplace=True)
-
-    style_stats.to_excel(excel_writer=excel_writer, sheet_name="stats")
-    _adjust_max_col_width(stats_df, excel_writer, "stats")
-
-
 def save_xlsx(
     excel_writer: pd.ExcelWriter,
     results: dict[str, dict],
     slr: str,
-    strategies: list[str],
 ):
     with Progress() as progress:
         saving_progress = progress.add_task("[green]Saving...", total=len(results))
         with excel_writer:
             for i, (key, result) in enumerate(results.items()):
                 df = pd.DataFrame(data=result["data"], columns=result["columns"])
-
-                if "name" in df.columns:
-                    cols = df.columns.tolist()
-                    cols.remove("name")
-                    cols.insert(0, "name")
-                    df = df[cols]
 
                 df.to_excel(excel_writer=excel_writer, sheet_name=key, index=False)
                 _adjust_max_col_width(df, excel_writer, key)
@@ -129,18 +64,14 @@ def save_xlsx(
                     refresh=True,
                 )
 
-            overall_results = {
-                key: value for key, value in results.items() if key in strategies
-            }
-            _statistics_tab(overall_results, excel_writer, strategies)
             _graph_tab(slr, excel_writer)
 
         progress.remove_task(saving_progress)
 
 
-def get_results(slr: str, metrics: list[str]) -> tuple[dict, list[str]]:
-    strategies_used_queries = SideQueries.get_strategies_used_query(slr)
-    check_review_query = SideQueries.get_check_review_query(slr)
+def get_results(slr: str) -> dict[str, dict]:
+    strategies_used_queries = ResultQuery.get_strategies_used_query(slr)
+    check_review_query = ResultQuery.get_check_review_query(slr)
 
     with Session() as session:
         strategies_used = get_strategies_used(
@@ -148,25 +79,21 @@ def get_results(slr: str, metrics: list[str]) -> tuple[dict, list[str]]:
         )
 
         results: dict = {}
-        strategies: list[str] = []
 
         for tes, wes in product(*strategies_used.values()):
             result_query: ResultQuery = ResultQuery(
                 slr=slr,
                 tes=tes,
                 wes=wes,
-                bonus_metrics=metrics,
             )
 
-            queries = result_query.get_queries()
+            stmt = result_query.get_queries()
 
-            results.update(get_results_from_db(queries, session))
+            results.update(get_results_from_db(stmt, session))
 
-            strategies.append(f"{tes}-{wes}")
+        results.update(get_results_from_db(ResultQuery.get_qgs_query(slr), session))
 
-        results.update(get_results_from_db(result_query.get_qgs_query(), session))
-
-    return results, strategies
+    return results
 
 
 @app.command(help="Creates a Excel file based on the given Path and SLR.")
@@ -180,20 +107,11 @@ def save(
     slr: str = typer.Argument(
         ..., help="Name of the SLR the results will be extracted."
     ),
-    metrics: list[str] = typer.Option(
-        default=None,
-        help="Bonus metrics to order the results and generate bonus top 10 lists. "
-        f"Available metrics: {[f'`{i}`' for i in _AVAILABLE_METRICS]} "
-        f"(outside the defaults: {[f'`{i}`' for i in _DEFAULT_METRICS]})",
-        show_default=False,
-    ),
 ):
-    verify_metrics(metrics)
-
     print("Retrieving information from database...")
 
-    results, strategies = get_results(slr, metrics)
+    results = get_results(slr)
 
     excel_writer = pd.ExcelWriter(path / f"{slr}.xlsx", engine="xlsxwriter")
 
-    save_xlsx(excel_writer, results, slr, strategies)
+    save_xlsx(excel_writer, results, slr)
--- src/sesgx_cli/database/util/results_queries.py
+++ src/sesgx_cli/database/util/results_queries.py
@@ -3,224 +3,80 @@ class StrategyBaseQueryNotImplemented(Exception):
 
 
 class ResultQuery:
-    """Class for wrapping all the queries need to retrieve information from the experiments' results.
+    """Class for wrapping all the queries needed to retrieve information from the experiments' results.
 
     Args:
         - slr (str): the SLR name;
         - ews (str): the enrichment word strategy;
         - tes (str): the topic extraction strategy;
-        - bonus_metrics (list[str] | None): a list of bonus metrics to be added to the default ones;
-        - row_num (int): the row number to be used in the top ten queries. QUERY NOT IN USE.
     """
 
-    def __init__(
-        self,
-        slr: str,
-        wes: str,
-        tes: str,
-        bonus_metrics: list[str] | None = None,
-        row_num: int = 1,
-    ):
-        self._qgs_query: str = f"""
-                select 
-                    e."name", 
-                    s.id, 
-                    s.title
-                from experiment_qgs eq 
-                left join experiment e on e.id = eq.experiment_id 
-                left join study s on s.id = eq.study_id 
-                left join slr on slr.id = e.slr_id 
-                where slr."name" = '{slr}';
-        """
-
-        self._results_queries: dict[str, str] = {
-            f"lda-{wes}": f"""select distinct on (ssp.search_string_id) ssp.search_string_id,
-                        ssp.start_set_precision,
-                        ssp.start_set_recall,
-                        ssp.start_set_f1_score,
-                        ssp.bsb_recall,
-                        ssp.sb_recall,
-                        lp.min_document_frequency as min_df,
-                        lp.n_topics as n_topics,
-                        fp.n_enrichments_per_word as n_similar_w,
-                        fp.n_words_per_topic as n_w_per_topic,
-                        ssp.n_scopus_results as n_scopus_results,
-                        ssp.n_qgs_in_scopus as n_qgs_studies_in_scopus,
-                        ssp.n_gs_in_scopus as n_gs_studies_in_scopus,
-                        e."name"
-                    from search_string_performance ssp 
-                        join params p on p.search_string_id = ssp.search_string_id 
-                        join experiment e on e.id = p.experiment_id 
-                        join slr s on s.id = e.slr_id 
-                        join formulation_params fp on fp.id = p.formulation_params_id
-                        left join lda_params lp on lp.id = p.lda_params_id
-                    where s."name" = '{slr}' and p.word_enrichment_strategy = '{wes}'""",
-            f"bt-{wes}": f"""select distinct on (ssp.search_string_id) ssp.search_string_id,
-                        ssp.start_set_precision,
-                        ssp.start_set_recall,
-                        ssp.start_set_f1_score,
-                        ssp.bsb_recall,
-                        ssp.sb_recall,
-                        bp.kmeans_n_clusters as n_clusters,
-                        bp.umap_n_neighbors as n_neighbors,
-                        fp.n_enrichments_per_word as n_similar_w,
-                        fp.n_words_per_topic as n_w_per_topic,
-                        ssp.n_scopus_results as n_scopus_results,
-                        ssp.n_qgs_in_scopus as n_qgs_studies_in_scopus,
-                        ssp.n_gs_in_scopus as n_gs_studies_in_scopus,
-                        e."name"
-                    from search_string_performance ssp 
-                        join params p on p.search_string_id = ssp.search_string_id 
-                        join experiment e on e.id = p.experiment_id 
-                        join slr s on s.id = e.slr_id 
-                        join formulation_params fp on fp.id = p.formulation_params_id
-                        left join bertopic_params bp on bp.id = p.bertopic_params_id
-                    where s."name" = '{slr}' and p.word_enrichment_strategy = '{wes}'""",
-        }
-
-        self._results_queries_by_row: dict[str, str] = {
-            f"lda-{wes}": f"""select 
-                            ssp.search_string_id as search_string_id,
-                            ssp.start_set_precision,
-                            ssp.start_set_recall,
-                            ssp.start_set_f1_score,
-                            ssp.bsb_recall,
-                            ssp.sb_recall,
-                            lp.min_document_frequency as min_df,
-                            lp.n_topics as n_topics,
-                            fp.n_enrichments_per_word as n_similar_w,
-                            fp.n_words_per_topic as n_w_per_topic,
-                            ssp.n_scopus_results as n_scopus_results,
-                            ssp.n_qgs_in_scopus as n_qgs_studies_in_scopus,
-                            ssp.n_gs_in_scopus as n_gs_studies_in_scopus,
-                            e."name",
-                            ROW_NUMBER() over (partition by e."name" order by ssp.{'placeholder'} desc) as row_num
-                        from search_string_performance ssp 
-                            join params p on p.search_string_id = ssp.search_string_id 
-                            join experiment e on e.id = p.experiment_id 
-                            join slr s on s.id = e.slr_id 
-                            join formulation_params fp on fp.id = p.formulation_params_id
-                            left join lda_params lp on lp.id = p.lda_params_id
-                        where 
-                            s."name" = '{slr}' and p.word_enrichment_strategy like '{wes}'""",
-            f"bt-{wes}": f"""select
-                            ssp.search_string_id as search_string_id,
-                            ssp.start_set_precision,
-                            ssp.start_set_recall,
-                            ssp.start_set_f1_score,
-                            ssp.bsb_recall,
-                            ssp.sb_recall,
-                            bp.kmeans_n_clusters as n_clusters,
-                            bp.umap_n_neighbors as n_neighbors,
-                            fp.n_enrichments_per_word as n_similar_w,
-                            fp.n_words_per_topic as n_w_per_topic,
-                            ssp.n_scopus_results as n_scopus_results,
-                            ssp.n_qgs_in_scopus as n_qgs_studies_in_scopus,
-                            ssp.n_gs_in_scopus as n_gs_studies_in_scopus,
-                            e."name",
-                            ROW_NUMBER() over (partition by e."name" order by ssp.{'placeholder'} desc) as row_num
-                        from search_string_performance ssp 
-                            join params p on p.search_string_id = ssp.search_string_id 
-                            join experiment e on e.id = p.experiment_id 
-                            join slr s on s.id = e.slr_id 
-                            join formulation_params fp on fp.id = p.formulation_params_id
-                            left join bertopic_params bp on bp.id = p.bertopic_params_id
-                        where 
-                            s."name" = '{slr}' and p.word_enrichment_strategy like '{wes}'""",
-        }
-
+    def __init__(self, slr: str, wes: str, tes: str):
         self._slr: str = slr
-        self._row_num: int = row_num
-        self.sws: str = wes
+        self.wes: str = wes
         self.tes: str = tes
 
-        self._set_metrics(bonus_metrics)
-
-    def _set_metrics(self, bonus_metrics: list[str] | None):
-        self._metrics: list[str] = ["start_set_precision", "start_set_recall"]
-
-        if bonus_metrics:
-            self._metrics.extend(bonus_metrics)
-
-    def _generate_top_ten_query(self, metric: str) -> str:
-        """
-        Generates queries for the top 10 best strings according to the strategy base query
-        and the metric to order the results.
-
-        Args:.
-            metric: metrics available to order the results.
-
-        Returns: the query added of the strategy base query as subquery, the order by and limit statements.
-
-        """
-        strategy_query = self._results_queries.get(f"{self.tes}-{self.sws}", None)
-
-        if not strategy_query:
-            raise StrategyBaseQueryNotImplemented()
-
-        return (
-            f"select * from ({strategy_query}) results "
-            f"order by {metric} desc limit 10"
-        )
-
-    def get_queries(self) -> dict[str, str]:
-        """
-        Generates a dictionary with all the queries needed to compose the Excel file for analysis.
-
-        Returns: A dict with the queries needed, they are:
-            - {strategy}: all the {strategy} results;
-            - qgs: all the experiments' QGSs;
-            - top_ten_{strategy}_{metric}: top ten {strategy} results ordered by the {metric};
-
-        """
-        key: str = f"{self.tes}-{self.sws}"
-
-        result_queries: dict = {}
-        result_queries[key] = self._results_queries[key]
-
-        if len(self.sws) > 4:
-            sws_reduced_name = self.sws[:3]
-        else:
-            sws_reduced_name = self.sws
-
-        if len(self.tes) > 4:
-            tes_reduced_name = self.tes[:3]
-        else:
-            tes_reduced_name = self.tes
-
-        for metric in self._metrics:
-            metric_reduced_name = metric.replace("start_set_", "ss_")
-            key: str = (
-                f"t10_({tes_reduced_name}-{sws_reduced_name})_{metric_reduced_name}"
-            )
-            result_queries[key] = self._generate_top_ten_query(metric)
-
-        return result_queries
-
-    def get_qgs_query(self) -> dict[str, str]:
-        return {"qgs": self._qgs_query}
-
-
-class SideQueries:
-    """Class with other queries related to the experiments' results."""
-
-    @staticmethod
-    def get_check_review_query(slr: str) -> str:
-        check_review: str = f"""
-        select
-            case 
-                when count(*)>0 then true
-                else false
-            end	
-        from experiment e 
-        join slr s on s.id = e.slr_id 
-        where s."name" = '{slr}'
-        """
-
-        return check_review
+        self._results_queries: dict[str, str] = {
+            f"lda-{self.wes}": f"""select
+                                e."name",
+                                ssp.search_string_id,
+                                ssp.start_set_precision,
+                                ssp.start_set_recall,
+                                ssp.start_set_f1_score,
+                                ssp.sb_recall,
+                                ssp.bsb_recall,
+                                ssp.n_scopus_results,
+                                ssp.n_qgs_in_scopus,
+                                ssp.n_gs_in_scopus,
+                                ssp.n_gs_in_bsb,
+                                ssp.n_gs_in_sb,
+                                fp.n_enrichments_per_word,
+                                fp.n_words_per_topic,
+                                lp.min_document_frequency as min_df,
+                                lp.n_topics
+                            from search_string_performance ssp
+                            left join params p on p.search_string_id = ssp.search_string_id
+                            left join formulation_params fp on fp.id = p.formulation_params_id 
+                            left join lda_params lp on lp.id = p.lda_params_id
+                            left join experiment e on e.id = p.experiment_id
+                            left join slr s on s.id = e.slr_id 
+                            where s."name" = '{self._slr}' and p.word_enrichment_strategy = '{self.wes}'
+                            order by p.id;""",
+            f"bt-{self.wes}": f"""select
+                                e."name",
+                                ssp.search_string_id,
+                                ssp.start_set_precision,
+                                ssp.start_set_recall,
+                                ssp.start_set_f1_score,
+                                ssp.sb_recall,
+                                ssp.bsb_recall,
+                                ssp.n_scopus_results,
+                                ssp.n_qgs_in_scopus,
+                                ssp.n_gs_in_scopus,
+                                ssp.n_gs_in_bsb,
+                                ssp.n_gs_in_sb,
+                                fp.n_enrichments_per_word,
+                                fp.n_words_per_topic,
+                                bp.kmeans_n_clusters,
+                                bp.umap_n_neighbors 
+                            from search_string_performance ssp
+                            left join params p on p.search_string_id = ssp.search_string_id
+                            left join formulation_params fp on fp.id = p.formulation_params_id 
+                            left join bertopic_params bp on bp.id = p.bertopic_params_id 
+                            left join experiment e on e.id = p.experiment_id
+                            left join slr s on s.id = e.slr_id 
+                            where s."name" = '{self._slr}' and p.word_enrichment_strategy = '{self.wes}'
+                            order by p.id;""",
+        }
 
     @staticmethod
     def get_strategies_used_query(slr: str) -> dict[str, str]:
+        """Get the queries to retrieve the strategies used in the experiments.
+
+        Args:
+            - slr (str): the SLR name;
+        """
+
         enrichment_strategies_query = f"""
         select 
             p.word_enrichment_strategy
@@ -250,3 +106,49 @@ class SideQueries:
             "tes": topic_extract_strategies_query,
             "sws": enrichment_strategies_query,
         }
+
+    @staticmethod
+    def get_check_review_query(slr: str) -> str:
+        """Get the query to check if the review exists.
+
+        Args:
+            - slr (str): the SLR name;
+        """
+        check_review_query: str = f"""
+        select 
+            p.word_enrichment_strategy
+        from params p 
+        left join experiment e ON e.id = p.experiment_id 
+        left join slr s on s.id = e.slr_id 
+        where s."name" like '{slr}'
+        group by p.word_enrichment_strategy;
+        """
+
+        return check_review_query
+
+    @staticmethod
+    def get_qgs_query(slr: str) -> dict[str, str]:
+        """Get the query to retrieve the QGS used in each experiment.
+
+        Args:
+            - slr (str): the SLR name;
+        """
+        _qgs_query: str = f"""
+        select 
+            e."name", 
+            s.id, 
+            s.title
+        from experiment_qgs eq 
+        left join experiment e on e.id = eq.experiment_id 
+        left join study s on s.id = eq.study_id 
+        left join slr on slr.id = e.slr_id 
+        where slr."name" = '{slr}';
+        """
+        return {"qgs": _qgs_query}
+
+    def get_queries(self) -> dict[str, str]:
+        """Get the queries to retrieve the results from each strategies used."""
+
+        key: str = f"{self.tes}-{self.wes}"
+
+        return {key: self._results_queries[key]}
